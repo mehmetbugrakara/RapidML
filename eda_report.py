b21b088b8b2306696ecbe1a9bb508919e838877e
Mehmet Buğra Kara <103603396+mehmetbugrakara@users.noreply.github.com>
2025-08-06 21:16:47 +0300

    Update eda_report.py

--- eda_report.py
+++ eda_report.py
@@ -6,7 +6,7 @@ import sweetviz as sv
 def generate_report(df, output_path: str):
     """Create and save Sweetviz report."""
     try:
-        print(df.head())  # Debugging line to check DataFrame content
+        # Debugging line to check DataFrame content
         report = sv.analyze(df)
         report.show_html(filepath=output_path, open_browser=False)
     except Exception as e:
